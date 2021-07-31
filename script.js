fe80d51855ee4a8e3e6b836bd1a11c1aa4dd9b05
ismizo <nerdyjinnyas@gmail.com>
2021-07-31 18:31:53 +0300

    added basic hiding generic page

--- script.js
+++ script.js
@@ -6,12 +6,24 @@ counters["elements"] ={};
 
 window.onload = () => {
   myScrollFunctions();
+  navClicks();
+  dropDownClicks();
 }
 
 
 function  myScrollFunctions() {
     document.onwheel = customScroll;
 };
+
+function navClicks(){
+    counters.elements["navLi"] = document.querySelectorAll("nav")[0].querySelectorAll("li");
+    counters.elements.navLi.forEach(element => {
+        element.addEventListener("click",()=>{
+            counters["navTemp"]= element.innerText;
+            navToItem(counters.navTemp);
+        })
+    })
+}
 
 
 function customScroll(event){
@@ -35,3 +47,52 @@ function customScroll(event){
     }
 
 }
+
+function emptyDisengagedFunc(){
+     //emptyfunction
+     return null;
+}
+
+
+function navToItem(eleText){
+    let tempVar = eleText;
+
+    if(tempVar==="Home"){
+        document.querySelectorAll(".internalboxone")[0].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+    }else if(tempVar==="About Us"){
+        document.querySelectorAll(".internalboxone")[1].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+
+    }else if(tempVar==="Destinations"){
+        document.querySelectorAll(".internalboxtwo")[1].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+
+    }else if(tempVar==="Stories & Updates"){
+        document.querySelectorAll(".internalboxtwo")[2].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+
+    }else if(tempVar==="Travel Guides"){
+        document.querySelectorAll(".internalboxtwo")[3].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+
+    }else if("Contact Us"){
+        document.querySelectorAll(".internalboxtwo")[4].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+
+    }else{
+        document.querySelectorAll(".internalboxone")[0].scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+    }
+
+}
+
+function dropDownClicks () {
+
+    counters.elements["dropdownbutts"] = document.querySelectorAll(".dropdownbuttons");
+    counters.elements.dropdownbutts.forEach(element => {
+        element.addEventListener("click",()=>{
+            rollOutGeneric(element.id);
+        })
+    })
+}
+
+
+function rollOutGeneric(elementId){
+    document.querySelectorAll(".genericbox")[0].style.height = "1000px";
+    document.querySelectorAll(".genericbox")[0].style.top = "0px";
+    document.onwheel = emptyDisengagedFunc;
+};
