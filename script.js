e66ec95a9afc2d66ba87a3091a043133e18f1c1f
ismizo <nerdyjinnyas@gmail.com>
2021-08-05 12:01:49 +0300

    added backend homeclick

--- script.js
+++ script.js
@@ -152,6 +152,7 @@ function checkTheURL () {
       counters.elements.cPan.remove();
       counters.elements.cPanGenericCont.remove();
       counters.elements.popUp.remove();
+      document.querySelectorAll("nav")[0].querySelectorAll("li")[0].addEventListener("click",()=>{window.location.url="./"})
       
      initiateLogInSetup(backendMatch);
     }else{
