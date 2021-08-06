621366af7bf886e561bcc49ac26d74eef5646eaa
ismizo <nerdyjinnyas@gmail.com>
2021-08-06 14:07:29 +0300

    antry

--- script.js
+++ script.js
@@ -1796,9 +1796,13 @@ async function startHailing(data,para,functionToRunAfter){
       }
     
       
-        let [myDate]    = new Date().toLocaleDateString("en-GB").split("/");
+        let [myDate]  = new Date().toLocaleDateString("en-GB").split("/");
         let [hour, minute, second] = new Date().toLocaleTimeString("en-US");
         let tempid =  myDate[0]+hour+minute+second;
+        console.log(myDate);
+        console.log(hour);
+        console.log(minute);
+        console.log(second);
     
        counters.localVar.cloudObj.contentObj.contentObj.draft.stories[0].storyObj = newStoryObj;
        counters.localVar.cloudObj.contentObj.contentObj.draft.stories[0].title = document.getElementById("editposttit").value;
